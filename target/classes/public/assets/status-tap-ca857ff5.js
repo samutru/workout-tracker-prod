2f5241dfe4ff497ecb71fcc3a36f2c146aeb9ef8
untereli <untereli@students.zhaw.ch>
2023-07-07 19:40:10 +0200

    inshallah

--- target/classes/public/assets/status-tap-ca857ff5.js
+++
@@ -1,3 +0,0 @@
-import{ah as r,ai as i,aj as c,ak as d,al as l}from"./index-e05d4bb7.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
