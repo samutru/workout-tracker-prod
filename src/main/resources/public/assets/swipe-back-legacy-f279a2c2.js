87d0e95b15381a480d995fb6aa26e0cc5597139a
untereli <untereli@students.zhaw.ch>
2023-07-08 10:42:11 +0200

    final commit

---
+++ src/main/resources/public/assets/swipe-back-legacy-f279a2c2.js
@@ -0,0 +1,5 @@
+System.register(["./index-legacy-a6dbd562.js"],(function(t,e){"use strict";var n,r,a;return{setters:[t=>{n=t.aa,r=t.ab,a=t.ac}],execute:function(){
+/*!
+       * (C) Ionic http://ionicframework.com - MIT License
+       */
+t("createSwipeBackGesture",((t,e,i,s,c)=>{const o=t.ownerDocument.defaultView;let u=n(t);const l=t=>u?-t.deltaX:t.deltaX;return r({el:t,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:r=>(u=n(t),(t=>{const{startX:e}=t;return u?e>=o.innerWidth-50:e<=50})(r)&&e()),onStart:i,onMove:t=>{const e=l(t)/o.innerWidth;s(e)},onEnd:t=>{const e=l(t),n=o.innerWidth,r=e/n,i=(t=>u?-t.velocityX:t.velocityX)(t),s=i>=0&&(i>.2||e>n/2),d=(s?1-r:r)*n;let h=0;if(d>5){const t=d/Math.abs(i);h=Math.min(t,540)}c(s,r<=0?.01:a(0,r,.9999),h)}})}))}}}));
---
+++ target/classes/public/assets/status-tap-19d27067.js
@@ -0,0 +1,3 @@
+import{ah as r,ai as i,aj as c,ak as d,al as l}from"./index-a9502547.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
